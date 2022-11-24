e8d216c80f57cb5e8aec4c7bafd18c8bf1b3d9e5
NikEgert <118725968+NikEgert@users.noreply.github.com>
2022-11-24 13:53:32 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+let count = 0
+let totalSum = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    totalSum += count
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    totalEl.textContent = "Total: " + totalSum
+    count = 0
+}
+
+
