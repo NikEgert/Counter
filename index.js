acb685c13b4031f8de66c3b7dbf6320b0b8c307c
NikEgert <118725968+NikEgert@users.noreply.github.com>
2022-11-24 13:46:05 +1100

    Added totalEL element

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let totalSum = 0
 
 function increment() {
     count += 1
@@ -11,5 +13,6 @@ function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
+    totalEl.textContent = "Total: " + totalSum
     count = 0
 }
